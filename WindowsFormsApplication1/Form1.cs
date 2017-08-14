d9e6e7bf4d39dfc9ec0623a6a2d7b546609f7495
tangliang <3042219692@qq.com>
2017-08-14 17:06:11 +0800

    初始化提交以添加默认 .gitIgnore 和 .gitAttribute 文件。

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        private Student stu;
+
+        private string formName;
+
+        public event EventHandler tellMe;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.eventLog1.Source = "My Server Explorer App";
+            try {
+                this.eventLog1.WriteEntry("Something happend", System.Diagnostics.EventLogEntryType.Information);
+            }
+            catch (Exception exception)
+            {
+                exception.GetBaseException();
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication1/SubScription.cs
@@ -0,0 +1,35 @@
+﻿using System;
+/// <summary>
+///    This is the brief description imformation of SubScription class
+/// </summary>
+     public class SubScription
+    {
+        public enum Status
+        {
+            Temporary,
+            Financial,
+            Unfinancial,
+            Suspended,
+            Test
+        }
+
+        public DateTime? PaidUpTo { get; set; }
+        //TODO: The Premeter will return the status value
+        public Status CurrentStatus
+        {
+            get
+            {
+                if (this.PaidUpTo.HasValue == false)
+                    return Status.Temporary;
+                if (this.PaidUpTo > DateTime.Today)
+                    return Status.Financial;
+                else
+                {
+                    if (this.PaidUpTo >= DateTime.Today.AddMonths(-3))
+                        return Status.Unfinancial;
+                    else
+                        return Status.Suspended;
+                } 
+            }
+        }
+    }
